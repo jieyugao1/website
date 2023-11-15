53f7c293c0b3886bd1c2c090056c049be00fdd06
jieyugao777 <49384105+jieyugao777@users.noreply.github.com>
2023-11-14 20:11:09 -0800

    update jmp

--- src/pages/Research/Research.jsx
+++ src/pages/Research/Research.jsx
@@ -12,7 +12,7 @@ const Research = (props) => {
                 <li>
                     <div style={{ fontWeight: 'bold' }} className={styles.cardHeader}>The Impact of Heteroskedasticity in Observational Studies of Causal Effects</div>
                     <div style={{ marginTop: '10px' }} className={styles.cardHeader}> With Ivan Jeliazkov</div>
-                    <p className={styles.abstractResearch}>There is a large and rapidly growing causal inference literature, yet little is known about the impact of heteroskedascity in popular causal settings. In observational studies where the presence of heteroskedasticity can not be ruled out with certainty, its effects in both treatment assignment and response generation must be studied. Our approach is Bayesian and involves specific modeling whose practical adequacy is then addressed through model comparisons. We build upon and extend existing methods for several well-known settngs such as sharp and fuzzy regression discontinuity designs, the potential outcomes framework, and propensity score matching. Key features of our approach in these settngs include flexible modeling and context-specific computationally efficient estimation algorithms, the ability to recover various functions of the treatment parameters, and an improved efficiency of estimation relative to alternatives that employ only a subset of the data in the analysis. Simulation studies are used to gauge the adequacy of the proposed methods, while their practical applicability is studied in three applications -- we examine the effect of academic probation on subsequent academic performance, the influence of Medigap on healthcare expenditures, and the impact of COVID-19 vaccination on mental wellbeing. These applications illustrate the consequences of misspecification and provide strong evidence that the presence of heteroskedascity should not be ignored.</p>
+                    <p className={styles.abstractResearch}>There is a large and rapidly growing causal inference literature, yet little is known about the impact of heteroskedasticity in popular causal settings. In observational studies where the presence of heteroskedasticity can not be ruled out with certainty, its effects in both treatment assignment and response generation must be studied. Our approach is Bayesian and involves specific modeling whose practical adequacy is then addressed through model comparisons. We build upon and extend existing methods for several well-known settings such as sharp and fuzzy regression discontinuity designs, the potential outcomes framework, and propensity score matching. Key features of our approach in these settings include flexible modeling and context-specific computationally efficient estimation algorithms, the ability to recover various functions of the treatment parameters, and an improved efficiency of estimation relative to alternatives that employ only a subset of the data in the analysis. Simulation studies are used to gauge the adequacy of the proposed methods, while their practical applicability is studied in three applications -- we examine the effect of academic probation on subsequent academic performance, the influence of Medigap on healthcare expenditures, and the impact of COVID-19 vaccination on mental well-being. These applications illustrate the consequences of misspecification and provide strong evidence that the presence of heteroskedasticity should not be ignored.</p>
                     <a href={JMP} target="_blank" rel="noreferrer" style={{ textAlign: "center" }}>Paper</a>
                 </li>
                 <li>
@@ -31,6 +31,14 @@ const Research = (props) => {
             <h2 className={styles.subh2}>Work in Progress</h2>
             <ul className={styles.list}>
                 <li>
+                    <div style={{ fontWeight: 'bold' }} className={styles.cardHeader}>Difference-in-Differences Estimation in the Presence of Heteroskedasticity</div>
+                    <div style={{ marginTop: '10px' }} className={styles.cardHeader}> With Ivan Jeliazkov</div>
+                </li>
+                <li>
+                    <div style={{ fontWeight: 'bold' }} className={styles.cardHeader}>Bayesian Semiparametric Instrumental Variables with Heteroskedasticity</div>
+                    <div style={{ marginTop: '10px' }} className={styles.cardHeader}> With Ivan Jeliazkov</div>
+                </li>
+                <li>
                     <div style={{ fontWeight: 'bold' }} className={styles.cardHeader}>Bayesian Analysis of Drug and Mental Health Treatment Effects</div>
                 </li>
                 <li>
@@ -38,6 +46,7 @@ const Research = (props) => {
                     <div style={{ marginTop: '10px' }} className={styles.cardHeader}> With Ivan Jeliazkov and Shubham Karnawat</div>
 
                 </li>
+
             </ul>
         </div >
     )
